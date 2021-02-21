fc865c7d77825170506784752ffd9573d2b404b5
wangechimk <wangechik.michelle@gmail.com>
2021-02-21 16:16:34 +0300

    styling changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,67 +1,19 @@
+const formObject = {};
+const maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+const femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
-var submission =function(){
-  var days of the week=[
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-  ];
-  var male names=[
-      "Kwasi",
-      "Kwadwo",
-      "Kwabena",
-      "Kwaku",
-      "Yaw",
-      "Kofi",
-      "Kwame",
-  ]
-  var female names=[
-    "Akosua",
-    "Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua",
-    "Ama",
-  ]
-  var year =parseInt(document.getElementById("year").value);
-  var  month=parseInt(document.getElementById("month").value);
-  var day=parseInt(document.getElementById("day").value);
-  var name=(document.getElementById("name").value);
-  var date of birth=new date(year+"/"+month+"/"+day);
-  var results=dateofbirth.getday();
-  var output =document.getElementById("output");
-  var male=document.getElementById("male")
-  var female=document.getElementById("female")
-
-  if(month=="" &&year=="" && day=="" && name==""){
-      alert(Fill in your credentials);
-      return false;
-  }
-  if (year<0){
-    output.style.background="teal"
-    output.style.color="white"
-    output.innerHTML="Hey!" + name + "Enter a valid month"
-  }
-  else if ((month<1) || (month>31)) {
-    output.style.background="teal"
-    output.style.color="white"
-    output.innerHTML="Hey!" + name + "Enter a valid month"
-  }
-else if(day<0|| day>31 ) {
-    output.style.background="teal"
-    output.style.color="white"
-    output.innerHTML="Hey!" + name + "Enter a valid month"
+// function to get all values form input fields
+let fetchInputValue = (e) => {
+  formObject[e.target.name] = e.target.value;
 }
- if (male.checked&& year>0 &&month>0 && month <12 && day >0 && day <32) {
-    output.style.background="red"
-    output.innerHTML="Hey!" +name +"you were born on a" +daysoftheweek[results]+"and your Akan name is" +malenames[results];
- }
- else if(female.checked&& year>0 &&month>0 && month <12 && day >0 && day <32) {
-    output.style.background="red"
-    output.innerHTML="Hey!" +name +"you were born on a" +daysoftheweek[results]+"and your Akan name is" +femalenames[results];
- }
-};
+
+let submitForm = () => {
+  // use input values to get a javascript date object
+  let date = new Date(parseInt(formObject.year), parseInt(formObject.month - 1), parseInt(formObject.dob));
+  let day = date.getDay();
+  let namesList;
+  formObject.gender === 'female' ? namesList = femaleNames : namesList = maleNames;
+  // console.log('NameSelected', namesList[day]);
+  document.getElementById('output').innerText = `Your name Ghanian akan name is: ${namesList[day]}`;
+}
+
